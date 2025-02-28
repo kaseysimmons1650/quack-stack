baadf9d86608072f5f8523279a5846ab9b67aed1
Chris Park <5chrispark5@gmail.com>
2025-02-28 15:12:00 -0500

    Forgot what I did

--- betting/betting.js
+++ betting/betting.js
@@ -1,4 +1,3 @@
-
 let classDict = {
   "AJ ZIMMERMAN": 1,
   "ANI UNDRAKONDA": 2,
@@ -26,56 +25,94 @@ let classDict = {
 };
 
 let raceWinner = Math.floor(Math.random() * 23) + 1;
-
+let balance = 100;
+const balanceText = document.getElementById("balance");
 document.getElementById("submitButton").addEventListener("click", function () {
+  const resText = document.getElementById("res-text");
+  resText.classList.add("hidden");
   var progressBar = document.querySelector(".progress-bar");
-  progressBar.style.width = "100%";
-  progressBar.setAttribute("aria-valuenow", 100);
+
   const duck = document.getElementById("duck").value.toUpperCase();
   const betAmount = document.getElementById("betAmount").value;
-  const resText = document.getElementById("res-text");
   const winAmount = betAmount * 3;
-  if (betAmount != undefined && duck != "") {
+  if (
+    betAmount != undefined &&
+    duck != "" &&
+    betAmount <= balance &&
+    checkNameDict(duck)
+  ) {
+    document.getElementById("nameError").style = "display:none";
+    progressBar.style.transition = "none";
+    progressBar.style.width = "0%";
+    progressBar.setAttribute("aria-valuenow", 0);
+    progressBar.offsetHeight;
+    progressBar.style.transition = "width 5s";
+    progressBar.style.width = "100%";
+    progressBar.setAttribute("aria-valuenow", 100);
     setTimeout(() => {
       resText.classList.remove("hidden");
       if (raceWinner == classDict[duck]) {
         resText.innerText = `Congrats, you have won ${winAmount} dollars!`;
         resText.classList.add("alert-primary");
+        balance += winAmount;
+        balanceText.innerText = `Total balance: $${balance}`;
       } else {
-        resText.innerHTML =
-          "<p>Sorry, you have lost all your money. Please call <a href='tel:+18005224700'>1-800-GAMBLER</a> for help</p>";
+        resText.innerHTML = `<p>Sorry, you have lost $${betAmount}. Please call <a href='tel:+18005224700'>1-800-GAMBLER</a> for help</p>`;
         resText.classList.add("alert-danger");
+        balance -= betAmount;
+        balanceText.innerText = `Total balance: $${balance}`;
       }
+      document.getElementById("submitButton").innerText = "Race again";
     }, 5000);
+  } else if (betAmount > balance) {
+    document.getElementById("betError").style = "display: inline; color:red";
   }
 
   console.log("Duck: " + duck);
   console.log("Bet Amount: " + betAmount);
 
-  console.log("Race Winner Number:"+raceWinner);
-  console.log("Duck Bet Number:"+classDict[duck]);
+  console.log("Race Winner Number:" + raceWinner);
+  console.log("Duck Bet Number:" + classDict[duck]);
   checkNameDict(duck);
-
 });
 
-
-function checkWinner(duckNumber){
-    if(duckNumber === raceWinner){
-        return true;
-    }
-    else{
-        return false;
-    }
+function checkWinner(duckNumber) {
+  if (duckNumber === raceWinner) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function checkNameDict(duckName){
-    duckName = duckName.toUpperCase();
-    for(duck in classDict){
-        if(duck === duckName){
-            return;
-        }
+function checkNameDict(duckName) {
+  duckName = duckName.toUpperCase();
+  for (duck in classDict) {
+    if (duck === duckName) {
+      return true;
     }
-    document.getElementById("nameError").style = "display: inline; color:red";
-
+  }
+  document.getElementById("nameError").style = "display: inline; color:red";
+  return false;
 }
 
+var modal = document.getElementById("depositModal");
+var btn = document.getElementById("depositBtn");
+var closeBtn = document.getElementById("closeBtn");
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+function depositMoney() {
+  let depositAmount = document.getElementById("depositAmount").value;
+  var depositInt = parseInt(depositAmount, 10);
+  balance += depositInt;
+  modal.style.display = "none";
+  document.getElementById("balance").innerText = `Total balance: $${balance}`;
+}
