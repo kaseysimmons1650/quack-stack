0214d6ff03bbb2360c3eeacafd8f2b5fc9f85390
Robin Pan <panrobin25@gmail.com>
2025-02-28 15:02:48 -0500

    Added text to progress bar and hiding text when betting again

--- betting/betting.js
+++ betting/betting.js
@@ -1,4 +1,3 @@
-
 let classDict = {
   "AJ ZIMMERMAN": 1,
   "ANI UNDRAKONDA": 2,
@@ -25,18 +24,31 @@ let classDict = {
   "CHRIS PENICK": 23,
 };
 
-let raceWinner = Math.floor(Math.random() * 23) + 1;
+// let raceWinner = Math.floor(Math.random() * 23) + 1;
 
 document.getElementById("submitButton").addEventListener("click", function () {
-  var progressBar = document.querySelector(".progress-bar");
-  progressBar.style.width = "100%";
-  progressBar.setAttribute("aria-valuenow", 100);
+  let raceWinner = Math.floor(Math.random() * 23) + 1;
+  // var progressBar = document.querySelector(".progress-bar");
+  // resetProgress(progressBar);
+  // progressBar.style.width = "100%";
+  // progressBar.setAttribute("aria-valuenow", 100);
   const duck = document.getElementById("duck").value.toUpperCase();
   const betAmount = document.getElementById("betAmount").value;
   const resText = document.getElementById("res-text");
   const winAmount = betAmount * 3;
+  const winner = Object.keys(classDict).find(key =>classDict[key] === raceWinner);
+
+  resText.classList.add("hidden");
+  resText.classList.remove("alert-primary");
+  resText.classList.remove("alert-danger");
+
   if (betAmount != undefined && duck != "") {
+    var progressBar = document.querySelector(".progress-bar");
+    resetProgress(progressBar);
+    progressBar.style.width = "100%";
+    progressBar.setAttribute("aria-valuenow", 100);
     setTimeout(() => {
+      progressBar.textContent = "Ducks done racing!";
       resText.classList.remove("hidden");
       if (raceWinner == classDict[duck]) {
         resText.innerText = `Congrats, you have won ${winAmount} dollars!`;
@@ -46,36 +58,41 @@ document.getElementById("submitButton").addEventListener("click", function () {
           "<p>Sorry, you have lost all your money. Please call <a href='tel:+18005224700'>1-800-GAMBLER</a> for help</p>";
         resText.classList.add("alert-danger");
       }
+      setTimeout(function(){
+        alert(`The winner is #${raceWinner} ${winner}`);
+      }, 100);
     }, 5000);
   }
 
   console.log("Duck: " + duck);
   console.log("Bet Amount: " + betAmount);
 
-  console.log("Race Winner Number:"+raceWinner);
-  console.log("Duck Bet Number:"+classDict[duck]);
+  console.log("Race Winner Number:" + raceWinner);
+  console.log("Duck Bet Number:" + classDict[duck]);
   checkNameDict(duck);
-
 });
 
-
-function checkWinner(duckNumber){
-    if(duckNumber === raceWinner){
-        return true;
-    }
-    else{
-        return false;
-    }
+function checkWinner(duckNumber) {
+  if (duckNumber === raceWinner) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function checkNameDict(duckName){
-    duckName = duckName.toUpperCase();
-    for(duck in classDict){
-        if(duck === duckName){
-            return;
-        }
+function checkNameDict(duckName) {
+  duckName = duckName.toUpperCase();
+  for (duck in classDict) {
+    if (duck === duckName) {
+      document.getElementById("nameError").style = "display: none; color:red";
+      return;
     }
-    document.getElementById("nameError").style = "display: inline; color:red";
-
+  }
+  document.getElementById("nameError").style = "display: inline; color:red";
 }
 
+function resetProgress(progressBar) {
+  progressBar.style.width = "0%";
+  progressBar.setAttribute("aria-valuenow", 0);
+  progressBar.textContent = "Ducks racing...";
+}
